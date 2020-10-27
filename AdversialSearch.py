e67083c6828d949fb40418ddf10fb92fa3e10d05
RayRCaringal <rayrcaringal@gmail.com>
2020-10-26 21:37:40 -0400

    Move fixed

--- AdversialSearch.py
+++ AdversialSearch.py
@@ -7,29 +7,29 @@ from pip._vendor.distlib.compat import raw_input
 def buildGrid(D):
     # Grid size is DxD
     # EE is Empty and TT is for Pit
-    grid = [["EE" for i in range(D)] for j in range(D)]
+    grid = [["EE " for i in range(D)] for j in range(D)]
 
     for col in range(1, D - 1):
         pits = (D / 3) - 1
         while pits != 0:
             row = random.randint(0, D - 1)
-            if grid[row][col] == "EE":
-                grid[row][col] = "TT"
+            if grid[row][col] == "EE ":
+                grid[row][col] = "TT "
                 pits -= 1
 
     count = 0;
     for row in range(0, D):
         if count == 0:
-            grid[0][row] = "AW"
-            grid[D - 1][row] = "PW"
+            grid[0][row] = "AW "
+            grid[D - 1][row] = "PW "
             count += 1
         elif count == 1:
-            grid[0][row] = "AH"
-            grid[D - 1][row] = "PH"
+            grid[0][row] = "AH "
+            grid[D - 1][row] = "PH "
             count += 1
         else:
-            grid[0][row] = "AM"
-            grid[D - 1][row] = "PM"
+            grid[0][row] = "AM "
+            grid[D - 1][row] = "PM "
             count = 0
 
     return grid
@@ -45,11 +45,11 @@ def selectValid(grid, D, user):
         row, col = tuple(map(int, raw_input().split(',')))
 
     cur = grid[row][col]
-    if cur[0] != user:
+    if cur[0] != user and cur[1] != user:
         print(cur)
         if cur[0] == "E":
             print("This is an empty cell")
-        elif cur[0] == "T":
+        elif cur[1] == "T":
             print("This is a pit")
         else:
             print("You have selected your opponents piece")
@@ -59,25 +59,41 @@ def selectValid(grid, D, user):
         return row, col
 
 
+# If someone has fallen in a pit TT is changed to T[User who fell in] if both users have fallen in it just becomes EE
 def move(cords, grid, D, user):
     print("Where would you like to move? Enter: (row,col)")
     cR, cC = cords
     nR, nC = tuple(map(int, raw_input().split(',')))
-    while not (isValid(nR, D)) or not (isValid(nC, D)) or distance(cR, nR, cC, nC) != 1:
+    while not (isValid(nR, D)) or not (isValid(nC, D)) or distance(nR, nC, cR, cC) != 1:
         if distance(cR, nR, cC, nC) != 1:
             print("Invalid coordinate, please input (row,col) within 1 cell of " + str(cords))
         else:
             print("Invalid coordinate, please input (row,col) within the bounds of 0 and " + str(D))
         nR, nC = tuple(map(int, raw_input().split(',')))
+
     next = grid[nR][nC]
     if next[0] == user:
         print("Invalid coordinate, you are trying to move into your own piece")
         move(cords, grid, D, user)
+    elif next[0] == "T":
+        if next[1] == user:       # Someone has already fallen
+            grid[nR][nC] = "T" + grid[cR][cC]
+        elif next[1] != "T":      # Both have fallen in so we change to EE
+            grid[nR][nC] = "EE "
+        else:                     # No one has fallen
+            grid[nR][nC] = "T" + user + " "
     else:
-        #If it's TT we would just delete it here and not replace, and if it's the opponent we will create a seperate method for that
-        grid[nR][nC] = grid[cR][cC]
-        grid[cR][cC] = "EE"
-        return grid
+        if list(grid[cR][cC])[0] == "T":
+            temp = grid[cR][cC]
+            grid[nR][nC] = temp[1:] + " "
+        else:
+            grid[nR][nC] = grid[cR][cC]
+
+    if list(grid[cR][cC])[0] == "T":
+        grid[cR][cC] = "T" + user + " "
+    else:
+        grid[cR][cC] = "EE "
+    return grid
 
 
 def isValid(index, D):
@@ -85,8 +101,7 @@ def isValid(index, D):
 
 
 def distance(x1, y1, x2, y2):
-    return math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2))
-
+    return int(math.sqrt((((x2 - x1) ** 2) + ((y2 - y1) ** 2))))
 
 def main():
     print("Input Grid Size")
@@ -98,9 +113,15 @@ def main():
     grid = buildGrid(6)
     print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
     cords = selectValid(grid, D, "P")
+    grid[4][5] = "TT "
     grid = move(cords, grid, D, "P")
     print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
-
+    cords = selectValid(grid, D, "P")
+    grid = move(cords, grid, D, "P")
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
+    cords = selectValid(grid, D, "P")
+    grid = move(cords, grid, D, "P")
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
 
 if __name__ == '__main__':
     main()
