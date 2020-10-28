e31aa7cd27a49440e77f3528e3a5cfb1ed62632c
RayRCaringal <rayrcaringal@gmail.com>
2020-10-27 23:42:33 -0400

    Added win and lose, not tested

--- AdversialSearch.py
+++ AdversialSearch.py
@@ -61,9 +61,11 @@ def selectValid(grid, D, user):
 
 # If someone has fallen in a pit TT is changed to T[User who fell in] if both users have fallen in it just becomes EE
 def move(cords, grid, D, user):
+    global playerScore, agentScore
     print("Where would you like to move? Enter: (row,col)")
     cR, cC = cords
     nR, nC = tuple(map(int, raw_input().split(',')))
+    curr = grid[cR][cC]
     while not (isValid(nR, D)) or not (isValid(nC, D)) or distance(nR, nC, cR, cC) != 1:
         if distance(cR, nR, cC, nC) != 1:
             print("Invalid coordinate, please input (row,col) within 1 cell of " + str(cords))
@@ -72,28 +74,84 @@ def move(cords, grid, D, user):
         nR, nC = tuple(map(int, raw_input().split(',')))
 
     next = grid[nR][nC]
+
+    # Decides what happens to the targeted cell
     if next[0] == user:
         print("Invalid coordinate, you are trying to move into your own piece")
         move(cords, grid, D, user)
+    # It is a trap!
     elif next[0] == "T":
-        if next[1] == user:       # Someone has already fallen
-            grid[nR][nC] = "T" + grid[cR][cC]
-        elif next[1] != "T":      # Both have fallen in so we change to EE
+        if next[1] == user:  # The user has already fallen so they just step over it
+            grid[nR][nC] = "T" + curr[0, 1]
+        elif next[1] != "T":  # The user falls, but at this point both have fallen in so we change to EE
+            if next[2] != " ":  # The user has found an opponent over a trapped space so they both die!  <This is kinda an assumption someone check up on this>
+                win(user)
             grid[nR][nC] = "EE "
-        else:                     # No one has fallen
+            lose(user)
+        else:  # No one has fallen, and the user falls in
             grid[nR][nC] = "T" + user + " "
-    else:
-        if list(grid[cR][cC])[0] == "T":
-            temp = grid[cR][cC]
-            grid[nR][nC] = temp[1:] + " "
+            lose(user)
+    # It is empty
+    elif next[0] == "E":
+        if curr == "T":
+            grid[nR][nC] = curr[1:] + " "
         else:
-            grid[nR][nC] = grid[cR][cC]
+            grid[nR][nC] = curr
+    # It is the opposing user
+    else:
+        if fight(curr[1], next[1]) == 0:
+            grid[nR][nC] == "EE "
+        elif fight(curr[1], next[1]) == 1:
+            grid[nR][nC] = curr
+            win(user)
+        else:
+            lose(user)
 
-    if list(grid[cR][cC])[0] == "T":
+    # Decides what current cell should be
+    if curr == "T":
         grid[cR][cC] = "T" + user + " "
     else:
         grid[cR][cC] = "EE "
     return grid
+
+
+# The user who wins causes the other user to lose points
+def win(user):
+    global playerScore, agentScore
+    if user == "P":
+        agentScore -= 1
+    else:
+        playerScore -= 1
+
+
+# Is Win but reversed
+def lose(user):
+    global playerScore, agentScore
+    if user == "P":
+        playerScore -= 1
+    else:
+        agentScore -= 1
+
+
+# 1 is Win, -1 is Lose, 0 is Tie
+def fight(user, opponent):
+    if user == opponent:
+        return 0
+    elif user == "W":
+        if opponent == "Hero":
+            return 1
+        else:
+            return -1
+    elif user == "H":
+        if opponent == "Mage":
+            return 1
+        else:
+            return -1
+    else:
+        if opponent == "W":
+            return 1
+        else:
+            return -1
 
 
 def isValid(index, D):
@@ -103,6 +161,7 @@ def isValid(index, D):
 def distance(x1, y1, x2, y2):
     return int(math.sqrt((((x2 - x1) ** 2) + ((y2 - y1) ** 2))))
 
+
 def main():
     print("Input Grid Size")
     D = int(input())
@@ -110,8 +169,11 @@ def main():
         print("Grid Size must be a multiple of 3 and greater than 0 ")
         D = int(input())
     print(D)
-    grid = buildGrid(6)
-    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
+    grid = buildGrid(D)
+    playerScore, agentScore = D, D
+
+
+"""    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
     cords = selectValid(grid, D, "P")
     grid[4][5] = "TT "
     grid = move(cords, grid, D, "P")
@@ -121,7 +183,7 @@ def main():
     print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
     cords = selectValid(grid, D, "P")
     grid = move(cords, grid, D, "P")
-    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in grid]))"""
 
 if __name__ == '__main__':
     main()
